fab8f77a7360c915dd63680d3cad98fa371183ee
Santiago Da Silva <sdasilva@outlook.es>
2020-10-17 16:07:13 -0300

    no es necesario importarlo de forma individual ya que se importa todo con * el persistence

--- ejbModule/com/entities/Area.java
+++ ejbModule/com/entities/Area.java
@@ -2,8 +2,6 @@ package com.entities;
 
 import java.io.Serializable;
 import javax.persistence.*;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
 
 /**
  * Entity implementation class for Entity: Area
