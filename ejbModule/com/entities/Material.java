7aaff97bcd3865abcaff3f8e20f167cf0e0fe21d
alvaro2431 <alvaro.leon@estudiantes.utec.edu.uy>
2020-10-17 01:25:59 -0300

    Update Material.java
    Material Alvaro

--- ejbModule/com/entities/Material.java
+++ ejbModule/com/entities/Material.java
@@ -14,8 +14,58 @@ public class Material implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
 
+	@Id
+	@GeneratedValue(strategy=GenerationType=Auto)
+	private Long id;
+	
+	@Column(length=40)
+	private String nombre;
+	
+	@Column(length=40)
+	private String descripcion;
+
+	
 	public Material() {
 		super();
 	}
-   
+
+
+	private Long getId() {
+		return id;
+	}
+
+
+	private void setId(Long id) {
+		this.id = id;
+	}
+
+
+	private String getNombre() {
+		return nombre;
+	}
+
+
+	private void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+
+	private String getDescripcion() {
+		return descripcion;
+	}
+
+
+	private void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+
+	private static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+
+
 }
+   
+
