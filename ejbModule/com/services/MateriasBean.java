cb5413344768ab99bf5398f790a01a706c566ed8
WildIndex <carlossuarez464@gmail.com>
2020-10-22 23:30:31 -0300

    creo materiasBean y remote

---
+++ ejbModule/com/services/MateriasBean.java
@@ -0,0 +1,18 @@
+package com.services;
+
+import javax.ejb.Stateless;
+
+/**
+ * Session Bean implementation class MateriasBean
+ */
+@Stateless
+public class MateriasBean implements MateriasBeanRemote {
+
+    /**
+     * Default constructor. 
+     */
+    public MateriasBean() {
+        // TODO Auto-generated constructor stub
+    }
+
+}
