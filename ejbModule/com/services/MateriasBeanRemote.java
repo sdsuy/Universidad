846ed87b47d7f8a8c31398922bc7179174d7f9d6
WildIndex <carlossuarez464@gmail.com>
2020-10-22 23:59:40 -0300

    pronta MateriasBeanRemote

--- ejbModule/com/services/MateriasBeanRemote.java
+++ ejbModule/com/services/MateriasBeanRemote.java
@@ -1,8 +1,19 @@
 package com.services;
 
+import java.util.List;
+
 import javax.ejb.Remote;
+
+import com.entities.Material;
+import com.exception.ServiciosException;
 
 @Remote
 public interface MateriasBeanRemote {
+	
+	public void crear(Material materia) throws ServiciosException;
+	public void actualizar(Material materia) throws ServiciosException;
+	public void borrar(Long id) throws ServiciosException;
+	public List<Material> obtenerTodos();
+	public List<Material> obtenerTodos(String filtro);
 
 }
