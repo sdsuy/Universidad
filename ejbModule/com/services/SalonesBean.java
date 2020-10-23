c86ad21da0bedba28f705a8efd73c95500039391
WildIndex <carlossuarez464@gmail.com>
2020-10-23 00:12:08 -0300

    creados salonesBean y beanremote vacios

---
+++ ejbModule/com/services/SalonesBean.java
@@ -0,0 +1,18 @@
+package com.services;
+
+import javax.ejb.Stateless;
+
+/**
+ * Session Bean implementation class SalonesBean
+ */
+@Stateless
+public class SalonesBean implements SalonesBeanRemote {
+
+    /**
+     * Default constructor. 
+     */
+    public SalonesBean() {
+        // TODO Auto-generated constructor stub
+    }
+
+}
