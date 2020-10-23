138c2bb46c86fec6fc502b748719cba67d5fef76
madgon <jose.madrid@estudiantes.utec.edu.uy>
2020-10-23 09:42:02 -0300

    SalonBean creado

--- ejbModule/com/services/SalonesBeanRemote.java
+++ ejbModule/com/services/SalonesBeanRemote.java
@@ -1,8 +1,18 @@
 package com.services;
 
+import java.util.List;
+
 import javax.ejb.Remote;
+
+import com.entities.Salon;
 
 @Remote
 public interface SalonesBeanRemote {
+	
+	void crear(Salon salon);
+	Salon leerUno(Long id); 
+	List<Salon> leerTodos();
+	void actualizar(Salon salon);
+	void borrar(Long id);
 
 }
