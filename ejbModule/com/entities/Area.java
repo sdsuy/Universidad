2cfaba3c544d604a54a5af04ba90c6cb5db9838d
WildIndex <carlossuarez464@gmail.com>
2020-10-24 14:46:24 -0300

    agrego getters y setters que faltaban

--- ejbModule/com/entities/Area.java
+++ ejbModule/com/entities/Area.java
@@ -32,5 +32,23 @@ public class Area implements Serializable {
 		this.id = id;
 		this.nombre = nombre;
 	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	
    
 }
