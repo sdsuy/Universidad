8a50197b2892446f0092c51015692a92134ba64a
Santiago Da Silva <sdasilva@outlook.es>
2020-10-25 17:11:30 -0300

    elimino lineas en blanco

--- ejbModule/com/entities/Material.java
+++ ejbModule/com/entities/Material.java
@@ -24,42 +24,34 @@ public class Material implements Serializable {
 	
 	@Column(length=200)
 	private String descripcion;
-
 	
 	public Material() {
 		super();
 	}
 
-
 	public Long getId() {
 		return id;
 	}
-
 
 	public void setId(Long id) {
 		this.id = id;
 	}
 
-
 	public String getNombre() {
 		return nombre;
 	}
-
 
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
 
-
 	public String getDescripcion() {
 		return descripcion;
 	}
 
-
 	public void setDescripcion(String descripcion) {
 		this.descripcion = descripcion;
 	}
-
 
 }
    
