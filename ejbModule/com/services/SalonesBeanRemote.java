c70e58104e678becf96a9bc652609979aa1212ae
Santiago Da Silva <sdasilva@outlook.es>
2020-10-24 21:19:26 -0300

    modifico firma

--- ejbModule/com/services/SalonesBeanRemote.java
+++ ejbModule/com/services/SalonesBeanRemote.java
@@ -10,7 +10,7 @@ import com.exception.ServiciosException;
 @Remote
 public interface SalonesBeanRemote {
 
-	void crear(Salon salon) throws ServiciosException;
+	void crear(String nombre, Long idDepartamento) throws ServiciosException;
 	void actualizar(Salon salon) throws ServiciosException;
 	void borrar(Long id) throws ServiciosException;
 	List<Salon> obtenerUno(String filtro);
